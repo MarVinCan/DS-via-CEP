75c33794841d5a09395f83fa20c5531a12332357
Marcus Vinicius <marcus.v.e.candido@gmail.com>
2024-01-19 13:23:03 -0300

    Criando o listController para inserir os cards

--- js/dsviacep.js
+++ js/dsviacep.js
@@ -1,3 +1,5 @@
 import * as formController from './controllers/from-controller.js';
+import * as listController from './controllers/list-controller.js';
 
 formController.init();
+listController.init();
